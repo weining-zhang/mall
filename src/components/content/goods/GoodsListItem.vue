148b3e4ee9957231026576c10370ea96b1051922
welnee <445103710@qq.com>
2020-07-27 20:27:52 +0800

    优化首页商品价格显示

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -3,7 +3,7 @@
     <img v-lazy="showImage" alt="" @load="imageLoad">
     <div class="goods-info">
       <p> {{goodsItem.title}} </p>
-      <span class="price"> {{goodsItem.price}} </span>
+      <span class="price"> ￥{{goodsItem.price}} </span>
       <span class="collect"> {{goodsItem.cfav}} </span>
     </div>
   </div>
